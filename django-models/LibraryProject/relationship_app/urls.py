84db0775495c7fab94e76ed5a4bf7e0ba01fb837
Goziee-git <prospachigoz@gmail.com>
2025-02-19 19:12:41 +0100

    mod for corrections in views.py  and urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from .views import list_books, LibraryDetailView, register, user_login, user_logout
 
 
 urlpatterns = [
