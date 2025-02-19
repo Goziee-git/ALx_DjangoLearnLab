66b202fd8a72d09c7c2d3df9dac09d49c5c4fd2b
Goziee-git <prospachigoz@gmail.com>
2025-02-19 16:56:59 +0100

    mod for views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 
 
