25ca82aebcb1128168b7fa935113ea41325a4895
Goziee-git <prospachigoz@gmail.com>
2025-02-24 14:36:59 +0100

    mod for urls and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register, list_books, LibraryDetailView, user_login, user_logout, admin_view, librarian_view, member_view
+from .views import register, list_books, LibraryDetailView, user_login, user_logout, Admin, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin/', admin_view, name='admin'),
+    path('admin/', Admin, name='admin'),
     path('librarian/', librarian_view, name='librarian'),
     path('member/', member_view, name='member'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -52,7 +52,7 @@ def is_admin(user):
    
 @login_required
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
    return render(request, 'relationship_app/admin.html')
 
 def is_librarian(user):
