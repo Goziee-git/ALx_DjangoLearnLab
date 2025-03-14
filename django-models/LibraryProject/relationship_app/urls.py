4806b4fe201f8226dfcc220edc283ee63976c29b
Goziee-git <prospachigoz@gmail.com>
2025-03-14 16:43:06 +0100

    mod for RBAC

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import user_passes_test
-from . import views 
-from . views.admin_view import check_admin_role, admin_view
-from . views.librarian_view import check_librarian_role, librarian_view
-from . views.member_view import check_member_role, member_view
-
+from . import views
 
 urlpatterns = [
     # Main application URLs
@@ -18,7 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html', next_page='login'), name='logout'), #uses the as_view method called on the built in LogoutView CBV
     
     # Role-based URLs
-    path('admin/', user_passes_test(check_admin_role)(views.admin_view), name='admin'),
-    path('librarian/', user_passes_test(check_librarian_role)(views.librarian_view), name='librarian'),
-    path('member/', user_passes_test(check_member_role)(views.member_view), name='member'),
+    path('admin/', user_passes_test(views.check_admin_role)(views.admin_view), name='admin'),
+    path('librarian/', user_passes_test(views.check_librarian_role)(views.librarian_view), name='librarian'),
+    path('member/', user_passes_test(views.check_member_role)(views.member_view), name='member'),
 ]
