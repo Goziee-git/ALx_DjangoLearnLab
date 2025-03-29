c879bb000eb542b9458d6d3cb50b255cb80f7a3a
Goziee-git <prospachigoz@gmail.com>
2025-03-29 11:41:47 +0100

    changes to first checker on week1

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -9,3 +9,19 @@ class Book(models.Model):
 
    def __str__(self):
       return self.title
+
+      
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+
+    objects = CustomUserManager()  # Attach the custom manager
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'custom_user'
+        verbose_name = 'Custom User'
+        verbose_name_plural = 'Custom Users'
